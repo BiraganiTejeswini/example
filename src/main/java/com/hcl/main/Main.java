6d9ce843d6bec71ba68521b49a83d09bcadece1a
BiraganiTejeswini <tejaswiyadav107@gmail.com>
2021-05-27 16:34:49 +0530

    first commit

---
+++ src/main/java/com/hcl/main/Main.java
@@ -0,0 +1,14 @@
+package com.hcl.main;
+
+import com.hcl.beans.Student;
+
+public class Main {
+public static void main(String[] args) {
+	Student s=new Student();
+	s.setId(1);
+	s.setName("teja");
+    System.out.println(s);
+
+	
+}
+}
